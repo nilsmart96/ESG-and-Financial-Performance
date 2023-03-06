70a88a570d316b31e38c76628188c659279c528d
Ian-Du <ianpeterdu@gmail.com>
2023-03-06 17:25:35 +0100

    Added classifiers for the returns and YoY
    Updated common to include other files

--- 2) Analysis/Common_Functions.py
+++ 2) Analysis/Common_Functions.py
@@ -1,2 +1,6 @@
+PATH_df_no_NAN = "../3) Data/main_df_no_NAN.csv"
 PATH_df_no_NAN_99p = "../3) Data/main_df_no_NAN_99p.csv"
 PATH_df_no_NAN_99p_YoY = "../3) Data/main_df_no_NAN_99p_YoY.csv"
+PATH_df_with_NAN = "../3) Data/main_df_with_NAN.csv"
+
+PATH_Refinitiv_ESG_Universe = "../3) Data/Refinitiv_ESG_Universe.csv"
